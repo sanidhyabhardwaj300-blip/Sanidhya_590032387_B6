5ad8aa0c9428eb801c44f15754caa8f057785f6f
Sanidhya Bhardwaj <sanidhyabhardwaj300@gmail.com>
2026-09-09 17:48:06 +0530

    submit

---
+++ Assignment 5/q1.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+{
+    int dd, mm, yy, D;
+    printf("Enter day, month and year: ");
+    scanf("%d %d %d", &dd, &mm, &yy);
+
+    if (yy > 0)
+    {
+        if (mm >= 1 && mm <= 12)
+        {
+            if (mm == 2)
+            {
+                if (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0))
+                {
+                    D = 29;
+                }
+                else
+                {
+                    D = 28;
+                }
+            }
+            else if (mm == 4 || mm == 6 || mm == 9 || mm == 11)
+            {
+                D = 30;
+            }
+            else
+            {
+                D = 31;
+            }
+
+            if (dd >= 1 && dd <= D)
+            {
+                printf("Valid date\n");
+            }
+            else
+            {
+                printf("Invalid day\n");
+            }
+        }
+        else
+        {
+            printf("Invalid month\n");
+        }
+    }
+    else
+    {
+        printf("Invalid year\n");
+    }
+
+    return 0;
+}
+
+
