6e1a3ef5adefd1af6304bc71c236310eb6d782ec
Sanidhya Bhardwaj <sanidhyabhardwaj300@gmail.com>
2026-09-16 16:52:26 +0530

    submit

--- Assignment 6/q2.c
+++ Assignment 6/q2.c
@@ -2,4 +2,18 @@
 int main()
 {
     
+    int a=0,b=1,c,i,n;
+    
+    printf("Enter an Integer to tell us how much term you need for Fibonacci Sequence:");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+
 }
